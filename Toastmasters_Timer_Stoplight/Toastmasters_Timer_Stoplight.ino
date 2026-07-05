514a93f7e326e850c2911e6b17eaab1d16047ee6
Kaleb Lee <klee.337x@gmail.com>
2026-07-05 15:11:28 -0700

    Add basic green, yellow, red LED cycle

--- Toastmasters_Timer_Stoplight/Toastmasters_Timer_Stoplight.ino
+++ Toastmasters_Timer_Stoplight/Toastmasters_Timer_Stoplight.ino
@@ -1,19 +1,27 @@
+// Define parameters
+int greenLight = 2;
+int yellowLight = 1;
+int redLight = 0;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
+  pinMode(greenLight, OUTPUT);
+  pinMode(yellowLight, OUTPUT);
+  pinMode(redLight, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int x = 0; x < 3; x++)
-  {
-    digitalWrite(x,HIGH);
-    delay(500);
-    digitalWrite(x,LOW);
-    delay(500);
-  }
+  digitalWrite(greenLight, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(greenLight, LOW);
+  digitalWrite(yellowLight, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(yellowLight, LOW);
+  digitalWrite(redLight, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(redLight, LOW);
+  delay(1000);
 
 }
